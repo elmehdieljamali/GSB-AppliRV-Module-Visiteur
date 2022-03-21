05142c8d574a4d3fd90c57f254fccc74af55c454
elmehdieljamali <elmehdi.eljamali.sio@gmail.com>
2022-03-21 10:41:13 +0100

    C1

---
+++ app/build/generated/source/buildConfig/debug/fr/gsb/rv/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package fr.gsb.rv;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "fr.gsb.rv";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
